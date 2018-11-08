9100cd9afe9c284ed93d01f4594326f0e1cb2a86
J. Victor Ferreira <jvictor.ferreira@jworks.eu>
2018-11-08 12:49:17 +0000

    Initial development commit (#1)
    * Initial development commit
    
    * Addressed comments made in PR

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node-js-environment-variable-setter
 This module can used in Node JS applications to mimic the environment variables used in a different environment
+
+To test this module, run the following commands:
+- npm install
+- npm test
---
+++ index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const fieldsToIgnoreByDefault = ["NODE_CONFIG_DIR"];
+
+const methods = {
+    setUpEnvironmentVariables: function (environmentVariableFilePath, fieldsToIgnore = fieldsToIgnoreByDefault) {
+        if (!fs.existsSync(environmentVariableFilePath)) {
+            throw new Error(`Could not find configuration file: ${environmentVariableFilePath}`);
+        }
+    
+        console.log(`Reading environment variables file: ${environmentVariableFilePath}`);
+        const environmentVariablesFromRemoteServer = JSON.parse(fs.readFileSync(environmentVariableFilePath));
+    
+        for (const fieldToIgnore of fieldsToIgnore) {
+            console.log(`Ignoring field ${fieldToIgnore}`);
+            delete environmentVariablesFromRemoteServer[fieldToIgnore];
+        }
+    
+        Object.assign(process.env, environmentVariablesFromRemoteServer);
+    }
+}
+
+module.exports = methods;
